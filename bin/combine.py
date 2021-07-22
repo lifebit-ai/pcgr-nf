c6a27f050175a10de7327365c5bda1ef16a25798
imendes93 <ines@lifebit.ai>
2021-07-22 15:19:06 +0100

    remove duplicated sample id column

--- bin/combine.py
+++ bin/combine.py
@@ -12,20 +12,19 @@ def __main__():
     if len(pcgr_tsvs) == 1:
         shutil.copyfile(pcgr_tsvs[0], "combined.tsv", follow_symlinks=True)
     else:
-        with open("combined.tsv", "w") as combined_fh:
+        with open("combined.tiers.tsv", "w") as combined_fh:
             for tsvfile in pcgr_tsvs:
-                samplename = os.path.basename(tsvfile).split('.')[0]
                 with open(tsvfile, "r") as tsv_fh:
                     #check if first file - only copy the header once
                     if pcgr_tsvs.index(tsvfile) == 0:
                         #get header line, add sample column at the start
-                        first_line = ['SAMPLE'] + tsv_fh.readline().split('\t')
+                        first_line = tsv_fh.readline().split('\t')
                         combined_fh.write('\t'.join(first_line))
                     else:
                         #skip first line
                         tsv_fh.readline()
                     for line in tsv_fh:
-                        line = [samplename] + line.split('\t')
+                        line = line.split('\t')
                         combined_fh.write('\t'.join(line))
 
 if __name__=="__main__": __main__()
